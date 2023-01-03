95cefae35bc0c1f687b10c84ea174cd184bf2960
mrbff <marco.baffo@live.it>
2023-01-03 11:41:39 +0100

    cleaner

--- srcs/client.c
+++ srcs/client.c
@@ -6,18 +6,18 @@
 /*   By: mabaffo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 19:42:11 by mabaffo           #+#    #+#             */
-/*   Updated: 2023/01/02 20:39:31 by mabaffo          ###   ########.fr       */
+/*   Updated: 2023/01/03 11:35:44 by mabaffo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void	ft_send_char(int pid_receiver, unsigned char c)
+void	ft_send(int pid_receiver, unsigned int c, int nbits)
 {
 	int	i;
 
 	i = 0;
-	while (i < 8)
+	while (i < nbits)
 	{
 		if (c % 2)
 			kill(pid_receiver, SIGUSR1);
@@ -25,24 +25,6 @@ void	ft_send_char(int pid_receiver, unsigned char c)
 			kill(pid_receiver, SIGUSR2);
 		if (c)
 			c /= 2;
-		i++;
-		usleep(50);
-	}
-}
-
-void	ft_send_pid(int pid_receiver, int pid_to_send)
-{
-	int	i;
-
-	i = 0;
-	while (i < (int)(sizeof(int) * 8))
-	{
-		if (pid_to_send % 2)
-			kill(pid_receiver, SIGUSR1);
-		else
-			kill(pid_receiver, SIGUSR2);
-		if (pid_to_send)
-			pid_to_send /= 2;
 		i++;
 		usleep(100);
 	}
@@ -66,14 +48,14 @@ int	main(int ac, char **av)
 	signal(SIGUSR1, ft_exit);
 	serverpid = ft_atoi(av[1]);
 	cpid = getpid();
-	ft_send_pid(serverpid, cpid);
+	ft_send(serverpid, cpid, sizeof(int) * 8);
 	i = 0;
 	while (av[2][i])
 	{
-		ft_send_char(serverpid, av[2][i]);
+		ft_send(serverpid, av[2][i], sizeof(char) * 8);
 		i++;
 	}
-	ft_send_char(serverpid, 0);
+	ft_send(serverpid, 0, sizeof(char) * 8);
 	pause();
 	return (0);
 }
